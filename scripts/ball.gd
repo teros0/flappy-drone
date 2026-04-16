0b9ad2483e0bb324b76d7b6b1a764dab34ae9321
Orest Petruniv <best.guess.oni@gmail.com>
2026-04-16 17:59:17 +0300

    add contact_monitor to the ball

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,12 +1,14 @@
 extends RigidBody2D
 
-@export var decay_on_wall_hit: float = 10
+@export var decay_on_wall_hit: float = 0.4
 @export var minimum_speed: float = 0.0
 
 func _ready():
 	# Disable gravity so the ball is fully driven by collisions / impulses
 	gravity_scale = 0.0
-
+	contact_monitor = true
+	max_contacts_reported = 8
+	
 	var world_wrapper := WorldWrapper.new()
 	add_child(world_wrapper)
 	print("Screen wrapping active for: ", name)
@@ -22,13 +24,11 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	while i < state.get_contact_count():
 		var collider: Object = state.get_contact_collider_object(i)
 		if collider and collider.is_in_group("squash_wall"):
-			print("here")
 			if decay_on_wall_hit > 0.0:
 				var v: Vector2 = state.linear_velocity
 				var speed: float = v.length()
 				if speed > 0.0:
 					var new_speed: float = max(speed * (1.0 - decay_on_wall_hit), minimum_speed)
-					print(new_speed)
 					if new_speed < speed:
 						state.linear_velocity = v.normalized() * new_speed
 			break
